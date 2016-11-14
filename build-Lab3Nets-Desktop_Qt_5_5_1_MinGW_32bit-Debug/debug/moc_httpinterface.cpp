5e6b352e238b4263a27e4c9b9553a2da374cb397
Vladimir Demchenko <Vladimir Demchenko>
2016-11-14 14:34:08 +0300

    хуйня всякая

--- build-Lab3Nets-Desktop_Qt_5_5_1_MinGW_32bit-Debug/debug/moc_httpinterface.cpp
+++ build-Lab3Nets-Desktop_Qt_5_5_1_MinGW_32bit-Debug/debug/moc_httpinterface.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_HTTPInterface_t {
-    QByteArrayData data[7];
-    char stringdata0[60];
+    QByteArrayData data[8];
+    char stringdata0[73];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,12 +34,14 @@ QT_MOC_LITERAL(1, 14, 4), // "load"
 QT_MOC_LITERAL(2, 19, 0), // ""
 QT_MOC_LITERAL(3, 20, 11), // "saveHistory"
 QT_MOC_LITERAL(4, 32, 13), // "updateHistory"
-QT_MOC_LITERAL(5, 46, 9), // "showError"
-QT_MOC_LITERAL(6, 56, 3) // "err"
+QT_MOC_LITERAL(5, 46, 12), // "clearHistory"
+QT_MOC_LITERAL(6, 59, 9), // "showError"
+QT_MOC_LITERAL(7, 69, 3) // "err"
 
     },
     "HTTPInterface\0load\0\0saveHistory\0"
-    "updateHistory\0showError\0err"
+    "updateHistory\0clearHistory\0showError\0"
+    "err"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +51,7 @@ static const uint qt_meta_data_HTTPInterface[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,16 +59,18 @@ static const uint qt_meta_data_HTTPInterface[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x08 /* Private */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    1,   37,    2, 0x08 /* Private */,
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    0,   42,    2, 0x08 /* Private */,
+       6,    1,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString,    6,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    7,
 
        0        // eod
 };
@@ -80,7 +84,8 @@ void HTTPInterface::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         case 0: _t->load(); break;
         case 1: _t->saveHistory(); break;
         case 2: _t->updateHistory(); break;
-        case 3: _t->showError((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 3: _t->clearHistory(); break;
+        case 4: _t->showError((*reinterpret_cast< QString(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -111,13 +116,13 @@ int HTTPInterface::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 5;
     }
     return _id;
 }
